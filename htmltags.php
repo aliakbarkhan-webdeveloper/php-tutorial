388adce87f5788f631e127c3c8ff95b6a6f60169
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-16 23:47:52 +0500

    commit

--- htmltags.php
+++ htmltags.php
@@ -1,1 +1,11 @@
-//when we use php it make  web dynamic
+<!-- we can make website dynamic by using php -->
+<?php
+$clr="style='color:orange'";
+echo "<h1 $clr>ALi Akbar Khan </h1>";
+
+?>
+
+<!-- writing code inside html tags -->
+ <h1><?php 
+ echo $clr
+ ?></h1>
