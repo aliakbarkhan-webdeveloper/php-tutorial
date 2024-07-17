9380e25789967d869dc264743283a55ae39822d9
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-17 21:54:01 +0500

    navbar

---
+++ navbar.php
@@ -0,0 +1,13 @@
+<?php
+$color="color:red;";
+echo "<nav>
+<ul style='list-style:none;display:flex; gap:3px; text-'>
+<li style=$color>Home</li>
+<li>contact</li>
+<li>portfolio</li>
+</ul>
+</nav>";
+
+
+
+?>
