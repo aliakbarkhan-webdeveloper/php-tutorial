f7b3cd69ffb45efe5b62168b36bb710065e85907
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-17 17:28:02 +0500

    arithmetic operators done

---
+++ arithmeticoperators.php
@@ -0,0 +1,24 @@
+<?php
+//Addition
+$a=7;
+$b=9;
+echo $a+$b;
+echo "<br>";
+//Multiplication
+echo $b*$a;
+echo "<br>";
+
+//Power/Exponential
+echo $b**2;
+echo "<br>";
+
+//subtract
+echo $b-$a;
+echo "<br>";
+//divide
+echo $b/3;
+echo "<br>";
+
+//Modular %
+echo $b%3;
+?>
