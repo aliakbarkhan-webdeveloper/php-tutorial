55c9753b7631b8adb65efea242830e94d2b10c71
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-17 18:50:32 +0500

    commit

--- comparisonoperator.php
+++ comparisonoperator.php
@@ -1,8 +1,21 @@
 <?php
 
+$a = 10;
+$b = 20;
 
+// ===
+echo var_dump($a === $b);
+echo "<br>";
 
+// >=
+echo var_dump($a<$b);
+echo "<br>";
 
+// >=
+echo var_dump($a!==$b);
+echo "<br>";
 
+// <=> spaceship
 
+echo var_dump($a<=>$b);
 ?>
