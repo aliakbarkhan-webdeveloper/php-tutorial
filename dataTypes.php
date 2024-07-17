f5563fe5016a9e792f3e9e6e8b765b937f389180
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-17 16:46:01 +0500

    commit

--- dataTypes.php
+++ dataTypes.php
@@ -8,44 +8,44 @@ echo "<br>";
 
 <!-- numbers -->
 <?php
-$number=123;
+$number = 123;
 echo $number;
 echo "<br>";
 ?>
 
 <!-- floats -->
 <?php
-$float=12.5;
+$float = 12.5;
 echo $float;
 echo "<br>";
 ?>
 
 <!-- boolean -->
 <?php
-$bool=true;
+$bool = true;
 echo $bool;
 echo "<br>";
 ?>
 <!-- Null -->
 <?php
-$null=null;
+$null = null;
 echo var_dump($null);
 echo "<br>";
 ?>
 <!-- Array -->
 <?php
-$arr=[1.4,"2",3];
+$arr = [1.4, "2", 3];
 echo var_dump($arr);
 echo "<br>";
 ?>
 
 <!-- Object -->
 <!-- <?php
-// $obj=new User();
-?> -->
+        // $obj=new User();
+        ?> -->
 
 <!-- Resources -->
- <?php
- $connection=ftp_connect("mongodb://localhost:27017");
- echo var_dump($connection);
- ?>
+<?php
+$connection = ftp_connect("mongodb://localhost:27017");
+echo var_dump($connection);
+?>
