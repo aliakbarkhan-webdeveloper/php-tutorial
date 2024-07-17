2309f8e48ad29f980ede2d61b0b69d33673b641d
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-17 18:25:40 +0500

    Assignment operators done

---
+++ assignment.php
@@ -0,0 +1,29 @@
+<?php
+
+//Reaasign by using =
+$var = "aLI";
+$var2 = $var;
+echo $var2;
+echo "<br>";
+//+=
+$var = 67;
+$var += 3;
+echo $var;
+echo "<br>";
+//subtract
+$var -= 1;
+echo $var;
+echo "<br>";
+//multiply
+$var /= 3;
+echo $var;
+echo "<br>";
+
+//modular
+$var %= 5;
+echo $var;
+echo "<br>";
+//exponential
+$var**=2;
+echo $var;
+?>
