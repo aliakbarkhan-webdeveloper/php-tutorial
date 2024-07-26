03c8b14c4e440e13663b169648221b17ae4780a9
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-26 23:34:42 +0500

    cookie-with-form

--- htmlform/cookies/cookieswithform.php
+++ htmlform/cookies/cookieswithform.php
@@ -1,11 +1,24 @@
 <form action="" method="post">
-    <input type="text" name="name" placeholder="name"><br><br><input type="text" name="user" placeholder="user name"><br><br>
+    <input type="text" name="name" placeholder="name"><br><br>
+    <input type="text" name="user" placeholder="user name"><br><br>
     <button type="submit" name="button" value="set">set cookie</button><br><br>
-    <button type="submit" name="button" value="display">get cookie</button><br><br><button type="submit" value="delete">delete cookie</button>
+    <button type="submit" name="button" value="display">get cookie</button><br><br>
+    <button type="submit" name="button" value="delete">delete cookie</button>
     
 </form>
-
-
 <?php
+if (isset($_POST["button"])) {
+    # code...
 
+    if ($_POST["button"]=="set") {
+        # code...
+
+        $val=$_POST["name"];
+        setcookie("cookie",$val);
+    }
+    if ($_POST["button"]=="delete") {
+        # code...
+        setcookie("cookie",null,-1);
+    }
+}
 ?>
