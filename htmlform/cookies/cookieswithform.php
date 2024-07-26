e8f782b9182e67fc60968c99e7a516f157e7cd83
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-07-26 23:11:12 +0500

    setting cookies by using forms

---
+++ htmlform/cookies/cookieswithform.php
@@ -0,0 +1,11 @@
+<form action="" method="post">
+    <input type="text" name="name" placeholder="name"><br><br><input type="text" name="user" placeholder="user name"><br><br>
+    <button type="submit" name="button" value="set">set cookie</button><br><br>
+    <button type="submit" name="button" value="display">get cookie</button><br><br><button type="submit" value="delete">delete cookie</button>
+    
+</form>
+
+
+<?php
+
+?>
